d396ca2f0314256114ab9b0bda1b8c2f6fab14c5
Herbert <34774005+Herbert-Karl@users.noreply.github.com>
2020-03-20 10:30:02 +0100

    added command for parsing and registered handler for it

--- src/extension.ts
+++ src/extension.ts
@@ -4,7 +4,16 @@ import * as vscode from 'vscode';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {}
+export function activate(context: vscode.ExtensionContext) {
+
+    let parseAsm = vscode.commands.registerCommand('digital-asm.parse-asm', () => {
+        console.log("profit");
+    });
+
+    // adding the implementation of the commands to the context of the extension, 
+    //so that the implementations will be executed, when the commands are called
+    context.subscriptions.push(parseAsm);
+}
 
 // this method is called when your extension is deactivated
 export function deactivate() {}
