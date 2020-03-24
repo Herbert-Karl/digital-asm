1d756880547e59b4dae1ae935c83e354d2af1700
Herbert <34774005+Herbert-Karl@users.noreply.github.com>
2020-03-24 10:06:34 +0100

    extrated checks to wrapper function

--- src/extension.ts
+++ src/extension.ts
@@ -18,7 +18,7 @@ export function activate(context: vscode.ExtensionContext) {
 
     // registering commands 
 
-    let parseAsm = vscode.commands.registerCommand('digital-asm.parse-asm', commandParseAsm);
+    let parseAsm = vscode.commands.registerCommand('digital-asm.parse-asm', commandFrame(commandParseAsm));
 
     // adding the implementation of the commands to the context of the extension, 
     //so that the implementations will be executed, when the commands are called
@@ -28,46 +28,22 @@ export function activate(context: vscode.ExtensionContext) {
 // this method is called when your extension is deactivated
 export function deactivate() {}
 
-// if there is an error, a error message is returned as string
+// function implementing the parsing of a .asm file into a .hex file
+// for this, the function uses an interface to java to create a Parser Object from asm3 for parsing the .asm file
+// and a HexFormatter Object for writing the parsed Program to file
+// params:
+// the function takes the file, that shall be parsed, as a TextDocument
+// return:
+// if there is an error, an error message is returned as string
 // if the function ended successful, `null` will be returned
-async function commandParseAsm(Uri: vscode.Uri): Promise<string | null> {
-    if(Uri===undefined) {
-        // if the command isnt given an URI for the file, which shall be parsed, we try to infer the file meant via the active editor
-        let help = vscode.window.activeTextEditor;
-        if(help===undefined) {
-            // if there is neither a given URI nor an active text editor, we can't parse and return while showing an error message
-            vscode.window.showErrorMessage("No given or active file.");
-            console.error("No given or active file.");
-            return "No given or active file.";
-        }
-        Uri = help.document.uri;
-    }
-
-    let fileToParse = await vscode.workspace.openTextDocument(Uri);
-    if(fileToParse.languageId!=='asm') {
-        // if the file, which shall be parsed, doesn't match the asm languageId, we don't try to parse it and return while showing an error message
-        vscode.window.showErrorMessage("Language of file isn't supported.");
-        console.error("Language of file isn't supported.");
-        return "Language of file isn't supported.";
-    }
-
-    if(!fs.existsSync(asm3JarPath)) {
-        // if we don't find a file at the given path for the asm3.jar, we alert the user and abort parsing
-        // continuing would creating errors when trying to access the classes contained in the jar file
-        vscode.window.showErrorMessage("Path for asm3.jar doesn't point to a existing file.");
-        console.error("Path for asm3.jar doesn't point to a existing file.");
-        return "Path for asm3.jar doesn't point to a existing file.";
-    }
-
-    java.classpath.push(path.join(asm3JarPath));
-
+function commandParseAsm(td: vscode.TextDocument): string | null {
     try {
         let Parser = java.import('de.neemann.assembler.parser.Parser');
-        let asmParser = new Parser(fileToParse.getText());
+        let asmParser = new Parser(td.getText());
         let program = asmParser.parseProgramSync(); // 'Sync' Suffix comes from the java module because reasons ...
         // because the writeHex Funtion in asm3 isnt public, we re-implement its functionality
         let HexFormatter = java.import('de.neemann.assembler.asm.formatter.HexFormatter');
-        let hexPath = Uri.path.replace(".asm", ".hex");
+        let hexPath = td.uri.path.replace(".asm", ".hex");
         let PrintStream = java.import('java.io.PrintStream');
         let hexFile = new PrintStream(hexPath);
         let hexFormatter = new HexFormatter(hexFile);
@@ -81,4 +57,47 @@ async function commandParseAsm(Uri: vscode.Uri): Promise<string | null> {
         return ex.message;
     }
     return null;
+}
+
+// function implementing checks before executing a given command 
+// params:
+// function implementing the command, which shall be executed with the given file
+// return:
+// an asyncronise function which wraps the command with checks for the given Uri, language of the File and the asm3 jar file
+function commandFrame(Command: (td: vscode.TextDocument) => string | null): (Uri: vscode.Uri, ) => Promise<string | null> {
+    return async function(Uri: vscode.Uri) {
+        if(Uri===undefined) {
+            // if the command isnt given an URI for the file, we try to infer the file meant via the active editor
+            let help = vscode.window.activeTextEditor;
+            if(help===undefined) {
+                // if there is neither a given URI nor an active text editor, we return while showing an error message
+                vscode.window.showErrorMessage("No given or active file.");
+                console.error("No given or active file.");
+                return "No given or active file.";
+            }
+            Uri = help.document.uri;
+        }
+
+        let fileToParse = await vscode.workspace.openTextDocument(Uri);
+
+        if(fileToParse.languageId!=='asm') {
+            // if the file doesn't match the asm languageId, we don't try to work with it and return while showing an error message
+            vscode.window.showErrorMessage("Language of file isn't supported.");
+            console.error("Language of file isn't supported.");
+            return "Language of file isn't supported.";
+        }
+
+        if(!fs.existsSync(asm3JarPath)) {
+            // if we don't find a file at the given path for the asm3.jar, we alert the user and abort
+            // continuing would creating errors when trying to access the classes contained in the jar file
+            vscode.window.showErrorMessage("Path for asm3.jar doesn't point to a existing file.");
+            console.error("Path for asm3.jar doesn't point to a existing file.");
+            return "Path for asm3.jar doesn't point to a existing file.";
+        }
+
+        java.classpath.push(path.join(asm3JarPath));
+
+        // execution of the given command
+        return Command(fileToParse);
+    };
 }
