26f7168e41f85a78479d66adc47203e142a234db
Herbert <34774005+Herbert-Karl@users.noreply.github.com>
2020-03-26 10:44:01 +0100

    started work on debug adapter

--- src/asmDebug.ts
+++ src/asmDebug.ts
@@ -1,10 +1,119 @@
-import { LoggingDebugSession } from 'vscode-debugadapter';
+import { DebugSession } from 'vscode-debugadapter';
 import { DebugProtocol } from 'vscode-debugprotocol';
 var java = require("java"); // used to interface with jar files
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-export class AsmDebugSession extends LoggingDebugSession {
+// the implementation of debugging for the asm files uses the DebugSession based on the Debug Adapter Protocol
+// thereby, the generic debug ui of VS Code will be usable for debugging
+// see https://github.com/microsoft/vscode-debugadapter-node/blob/master/adapter/src/debugSession.ts for the methods and variables
+export class AsmDebugSession extends DebugSession {
     
+    // because we do not support multiple threads, we hardcode an id to use as a default
+    private static THREAD_ID: 1;
+
+    public constructor() {
+        super();
+
+        // debugger uses zero-based lines and columns
+        this.setDebuggerLinesStartAt1(false);
+        this.setDebuggerColumnsStartAt1(false);
+
+    }
+
+    // override of the default implementation of the function
+    // first request to the Degub Adapter, when starting debugging
+    // Debug Adpater returns information about which capabilities it implements
+    // a implemented feature is signaled by setting the corresponding flag as true
+    protected initializeRequest(response: DebugProtocol.InitializeResponse, args: DebugProtocol.InitializeRequestArguments): void {
+        
+        // if the response body is undefined, we create it
+        response.body = response.body || {};
+        
+        // features, that aren't implemented, do not need there flags to be set to false
+        // flags without explicit value are interpreted as false
+        // check https://microsoft.github.io/debug-adapter-protocol/specification#capabilities for all possible capabilities
+        
+        // ...
+        response.body.supportsConfigurationDoneRequest = false;
+
+        // we support restart requests for the program
+        response.body.supportsRestartRequest = true;
+
+        // we do not support Terminating the program by the debug adapter
+        response.body.supportsTerminateRequest = false;
+
+        // we do not support requests to cancel earlier requests or progress sequences
+        response.body.supportsCancelRequest = false;
+
+        this.sendResponse(response);
+    }
+ 
+    // override of the default implementation of the function
+    // associated with the capability "supportsRestartRequest"
+    // response is empty / just an acknowledgement that the request has been done
+    protected restartRequest(response: DebugProtocol.RestartResponse, args: DebugProtocol.RestartArguments, request?: DebugProtocol.Request): void {
+        // ToDo: Implement!!!
+        this.sendResponse(response);
+	}
+
+    // override of the default implementation of the function
+    // sets the breakpoints for the program, previous breakpoints shall be discarded
+    // the request contains all expected breakpoints
+    // the response must return information about all actual created breakpoints
+    protected setBreakPointsRequest(response: DebugProtocol.SetBreakpointsResponse, args: DebugProtocol.SetBreakpointsArguments, request?: DebugProtocol.Request): void {
+        // ToDo: implement!!!
+        this.sendResponse(response);
+    }
+    
+    // override of the default implementation of the function
+    // (re)start running the program
+    protected continueRequest(response: DebugProtocol.ContinueResponse, args: DebugProtocol.ContinueArguments, request?: DebugProtocol.Request) : void {
+        // ToDo: Implement!!!
+        this.sendResponse(response);
+    }
+    
+    // override of the default implementation of the function
+    // running the program for one step
+    // the response is only an acknowledgement, so no content is required
+    protected nextRequest(response: DebugProtocol.NextResponse, args: DebugProtocol.NextArguments, request?: DebugProtocol.Request) : void {
+        // ToDo: Implement!!!
+        this.sendResponse(response);
+	}
+
+    // override of the default implementation of the function
+    // request for stepping into a function if possible
+    // because it isn't feasible for our language, we make one step instead
+    protected stepInRequest(response: DebugProtocol.StepInResponse, args: DebugProtocol.StepInArguments, request?: DebugProtocol.Request) : void {
+		this.nextRequest(response, args, request);
+	}
+    
+    // override of the default implementation of the function
+    // request for stepping out off a function if possible
+    // because it isn't feasible for our language, we make one step instead
+    protected stepOutRequest(response: DebugProtocol.StepOutResponse, args: DebugProtocol.StepOutArguments, request?: DebugProtocol.Request) : void {
+		this.nextRequest(response, args, request);
+	}
+
+    // override of the default implementation of the function
+    // request to stop executing the program
+    // the response is only an acknowledgement
+    protected pauseRequest(response: DebugProtocol.PauseResponse, args: DebugProtocol.PauseArguments, request?: DebugProtocol.Request) : void {
+        // ToDo: Implement!!!
+        this.sendResponse(response);
+	}
+
+    // override of the default implementation of the function
+    // request for the referenced source code
+    protected sourceRequest(response: DebugProtocol.SourceResponse, args: DebugProtocol.SourceArguments, request?: DebugProtocol.Request) : void {
+        // ToDo: implement!!!
+        this.sendResponse(response);
+	}
+
+    /*
+        requests for Evaluate, StackTraces, Scopes and Variables aren't implemented (empty base implementation used)
+        because we do not support these things
+        but does things can't be defined via the capabilities, so we can't explicitly forbid such requests
+    */
 }
