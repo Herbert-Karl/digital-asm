d569540f6664a1300f6503dd55b675641de7e3d2
Herbert <34774005+Herbert-Karl@users.noreply.github.com>
2020-03-27 20:15:21 +0100

    updated and commented remoteInterface + tests

--- src/remoteInterface.ts
+++ src/remoteInterface.ts
@@ -12,37 +12,49 @@ export class RemoteInterface {
         this.Port = port;
     }
 
-    // ...
+    // loading the program and starting it
+    // params:
+    // path to the hexFile, which shall be run in the simulator 
     public start(pathToHex: string) {
         this.sendRequest("start", pathToHex);
     }
 
-    // ...
+    // loading the program but does not begin running it
+    // params:
+    // path to the hexFile, which shall be run in the simulator
     public debug(pathToHex: string) {
         this.sendRequest("debug", pathToHex);
     }
 
-    // ...
-    public run(): Promise<string> {
+    // runs the program
+    // executing is stopped, when a BRK statement comes up
+    public async run(): Promise<string> {
         return this.sendRequest("run", "");
     }
 
-    // ...
+    // stops execution of the program
     public stop() {
         this.sendRequest("stop", "");
     }
 
-    // ...
-    public step(): Promise<string> {
+    // runs a single clock step in the simulator
+    public async step(): Promise<string> {
         return this.sendRequest("step", "");
     }
 
-
+    // function for sending commands and arguments to the digital simulator via tcp socket
+    // params:
+    // the command for the simulator
+    // and potentially needed arguments
+    // returns:
+    // a Promise which either resolves to a string returned by the simulator that contains the current address in the program hex code
+    // or rejects into an error. the error might come from the simulator itself or from the tcp connection
     private async sendRequest(command: string, args: string): Promise<string> {
         let socket = new net.Socket();
         socket.setDefaultEncoding("utf8");
         socket.connect(this.Port, this.IP);
-        let response: string = await new Promise((resolve, reject) => {
+
+        return new Promise<string>((resolve, reject) => {
             if(args!=="") {
                 command = command+":"+args;
             }
@@ -51,7 +63,12 @@ export class RemoteInterface {
             // when the socket gets the data, we end the socket connection and return the data from the connection
             socket.on('data', function(data) {
                 socket.destroy();
-                resolve(data.toString('utf8'));
+                let response = data.toString('utf8');
+                // checking the returned data from the simulator for an okay signal
+                if(!(response==="ok" || response.startsWith("ok:"))) {
+                    reject(new Error("Error received from simulator: " + response));
+                }
+                resolve(response);
             });
 
             socket.on('error', function(err) {
@@ -59,10 +76,6 @@ export class RemoteInterface {
                 reject(err);
             });
         });
-        if(response!=="ok" || !response.startsWith("ok:")) {
-            //ToDo: error
-        }
-        return response;
     }
 
 }
--- src/test/suite/remoteInterface.test.ts
+++ src/test/suite/remoteInterface.test.ts
@@ -20,21 +20,51 @@ suite('RemoteInterface Test Suite', () => {
 	  });
 
 	test('Sending commands', async () => {
-            const hexFile = path.join(__dirname + testFolderLocation + 'example_parsed.hex');
-            // for the test we create our own small tcp listeing server
-            let server = net.createServer();
-            server.listen(27322, "localhost");
-            // we simple answer with the same string for every connection
-            server.on('connection', function(socket) {
-                socket.write("ok:001f");
-            });
-
-            let remoteInterface = new RemoteInterface("localhost", 27322);
-            assert.equal(await remoteInterface.step(), "ok:001f");
-            assert.equal(await remoteInterface.run(), "ok:001f");
-
-            // cleanup
-            server.close();
+		// for the test we create our own small tcp listeing server
+		let server = net.createServer();
+		server.listen(27322, "localhost");
+		// we simple answer with the same string for every connection
+		server.on('connection', function(socket) {
+			socket.write("ok:001f");
 		});
+
+		let remoteInterface = new RemoteInterface("localhost", 27322);
+		assert.equal(await remoteInterface.step(), "ok:001f");
+		assert.equal(await remoteInterface.run(), "ok:001f");
+
+		// cleanup
+		server.close();
+	});
+
+    test('Sending wrong data', async () => {
+		// for the test we create our own small tcp listeing server
+		let server = net.createServer();
+		server.listen(27323, "localhost");
+		// we simple answer with the same string for every connection
+		server.on('connection', function(socket) {
+			socket.write("this is just wrong");
+		});
+
+		let remoteInterface = new RemoteInterface("localhost", 27323);
+		let response = await remoteInterface.run().catch((err)=>{ assert.equal(err.message, "Error received from simulator: "+"this is just wrong"); });
+
+		if(response!==undefined) {
+			assert.fail("the function shouldn't have returned a value");
+		}
+
+		// cleanup
+		server.close();
+	});
+
+	test('No Server for connection', async () => {
+		const hexFile = path.join(__dirname + testFolderLocation + 'example_parsed.hex');
+
+		let remoteInterface = new RemoteInterface("localhost", 27325);
+		let response = await remoteInterface.run().catch((err)=>{ assert.equal(err.code, "ECONNREFUSED"); });
+
+		if(response!==undefined) {
+			assert.fail("the function shouldn't have returned a value");
+		}
+    });
 
 });
