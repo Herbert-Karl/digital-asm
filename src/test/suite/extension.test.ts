85a9b38b207cfaf7110cdc357d3b71b860045c8d
Herbert <34774005+Herbert-Karl@users.noreply.github.com>
2020-04-08 20:32:43 +0200

    restructured parsing tests and inclided .map files

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -11,87 +11,112 @@ import * as vscode from 'vscode';
 const testFolderLocation = '/../../../src/test/examples/';
 
 suite('Extension Test Suite', () => {
+	
+	let asmExampleFile: vscode.Uri;
+	let hexExampleFile: vscode.Uri;
+	let lstExampleFile: vscode.Uri;
+	let mapExampleFile: vscode.Uri;
+	let parsedExampleFile: vscode.Uri;
+	let createdExampleListingFile: vscode.Uri;
+	let createdExampleMappingFile: vscode.Uri;
+	//
+	let asmTestFile: vscode.Uri;
+	let hexTestFile: vscode.Uri;
+	let lstTestFile: vscode.Uri;
+	let mapTestFile: vscode.Uri;
+	let parsedTestFile: vscode.Uri;
+	let createdTestListingFile: vscode.Uri;
+	let createdTestMappingFile: vscode.Uri;
+
 	before(() => {
 		vscode.window.showInformationMessage('Start all tests.');
+		
+		// defining URIs for the files used in the tests
+		asmExampleFile = vscode.Uri.file(path.join(__dirname + testFolderLocation + 'example.asm'));
+	 	hexExampleFile = vscode.Uri.file(path.join(__dirname + testFolderLocation + 'example_parsed.hex'));
+		lstExampleFile = vscode.Uri.file(path.join(__dirname + testFolderLocation + 'example_listing.lst'));
+		mapExampleFile = vscode.Uri.file(path.join(__dirname + testFolderLocation + 'example_mapping.map'));
+		parsedExampleFile = vscode.Uri.file(path.join(__dirname + testFolderLocation + 'example.hex'));
+		createdExampleListingFile = vscode.Uri.file(path.join(__dirname + testFolderLocation + 'example.lst'));
+		createdExampleMappingFile = vscode.Uri.file(path.join(__dirname + testFolderLocation + 'example.map'));
+		// 
+		asmTestFile = vscode.Uri.file(path.join(__dirname + testFolderLocation + 'test.asm'));
+		hexTestFile = vscode.Uri.file(path.join(__dirname + testFolderLocation + 'test_parsed.hex'));
+		lstTestFile = vscode.Uri.file(path.join(__dirname + testFolderLocation + 'test_listing.lst'));
+		mapTestFile = vscode.Uri.file(path.join(__dirname + testFolderLocation + 'test_mapping.map'));
+		parsedTestFile = vscode.Uri.file(path.join(__dirname + testFolderLocation + 'test.hex'));
+		createdTestListingFile = vscode.Uri.file(path.join(__dirname + testFolderLocation + 'test.lst'));
+		createdTestMappingFile = vscode.Uri.file(path.join(__dirname + testFolderLocation + 'test.map'));
+
+		// if an output file is present before we parse one in the tests, we remove it
+		if(fs.existsSync(parsedExampleFile.fsPath)) { fs.unlinkSync(parsedExampleFile.fsPath); }
+		if(fs.existsSync(createdExampleListingFile.fsPath)) { fs.unlinkSync(createdExampleListingFile.fsPath); }
+		if(fs.existsSync(createdExampleMappingFile.fsPath)) { fs.unlinkSync(createdExampleMappingFile.fsPath); }
+		//
+		if(fs.existsSync(parsedTestFile.fsPath)) { fs.unlinkSync(parsedTestFile.fsPath); }
+		if(fs.existsSync(createdTestListingFile.fsPath)) { fs.unlinkSync(createdTestListingFile.fsPath); }
+		if(fs.existsSync(createdTestMappingFile.fsPath)) { fs.unlinkSync(createdTestMappingFile.fsPath); }
+
 	});
+	
 	after(() => {
+		// cleanup
+		fs.unlinkSync(parsedExampleFile.fsPath); // removes the output file from this test; subsequent tests should start without a parsed .hex file
+		fs.unlinkSync(createdExampleListingFile.fsPath); // removes the output file from this test; subsequent tests should be influenced
+		fs.unlinkSync(createdExampleMappingFile.fsPath); // removes the output file from this test; subsequent tests should be influenced
+		// 
+		fs.unlinkSync(parsedTestFile.fsPath); // removes the output file from this test; subsequent tests should start without a parsed .hex file
+		fs.unlinkSync(createdTestListingFile.fsPath); // removes the output file from this test; subsequent tests should be influenced
+		fs.unlinkSync(createdTestMappingFile.fsPath); // removes the output file from this test; subsequent tests should be influenced
+	  
 		vscode.window.showInformationMessage('All tests done!');
-	  });
+	});
 
 	test('Parsing simple asm file', async () => {
-		// defining URIs for the files used in this test
-		const asmFile = vscode.Uri.file(path.join(__dirname + testFolderLocation + 'example.asm'));
-		const hexFile = vscode.Uri.file(path.join(__dirname + testFolderLocation + 'example_parsed.hex'));
-		const lstFile = vscode.Uri.file(path.join(__dirname + testFolderLocation + 'example_listing.lst'));
-		const parsedFile = vscode.Uri.file(path.join(__dirname + testFolderLocation + 'example.hex'));
-		const createdListingFile = vscode.Uri.file(path.join(__dirname + testFolderLocation + 'example.lst'));
-
-		if(fs.existsSync(parsedFile.fsPath)) {
-			// if an output file is present before we parse one in this test, we remove it
-			fs.unlinkSync(parsedFile.fsPath);
-		}
-		if(fs.existsSync(createdListingFile.fsPath)) {
-			// if an output file is present before we parse one in this test, we remove it
-			fs.unlinkSync(createdListingFile.fsPath);
-		}
-
-		const document = await vscode.workspace.openTextDocument(asmFile);
+		const document = await vscode.workspace.openTextDocument(asmExampleFile);
 		const editor = await vscode.window.showTextDocument(document);
 
-		let errorMessage = await vscode.commands.executeCommand('digital-asm.parse-asm', asmFile);
+		let errorMessage = await vscode.commands.executeCommand('digital-asm.parse-asm', asmExampleFile);
 
 		assert.ifError(errorMessage);
 
 		// getting the content of the files
-		let parsedContent = fs.readFileSync(parsedFile.fsPath, 'utf-8');
-		const expectedHexContent = fs.readFileSync(hexFile.fsPath, 'utf-8');
-		let listingContent = fs.readFileSync(createdListingFile.fsPath, 'utf-8');
-		const expectedLstContent = fs.readFileSync(lstFile.fsPath, 'utf-8');
+		let parsedContent = standardizeLineEnding(fs.readFileSync(parsedExampleFile.fsPath, 'utf-8'));
+		const expectedHexContent = standardizeLineEnding(fs.readFileSync(hexExampleFile.fsPath, 'utf-8'));
+		let listingContent = standardizeLineEnding(fs.readFileSync(createdExampleListingFile.fsPath, 'utf-8'));
+		const expectedLstContent = standardizeLineEnding(fs.readFileSync(lstExampleFile.fsPath, 'utf-8'));
+		let mappingContent = standardizeLineEnding(fs.readFileSync(createdExampleMappingFile.fsPath, 'utf-8'));
+		const expectedMapContent = standardizeLineEnding(fs.readFileSync(mapExampleFile.fsPath, 'utf-8'));
 
 		assert.deepEqual(parsedContent, expectedHexContent);
 		assert.deepEqual(listingContent, expectedLstContent);
-
-		// cleanup
-		fs.unlinkSync(parsedFile.fsPath); // removes the output file from this test; subsequent tests should start without a parsed .hex file
-		fs.unlinkSync(createdListingFile.fsPath); // removes the output file from this test; subsequent tests should be influenced
+		assert.deepEqual(mappingContent, expectedMapContent);
 	});
 
 	test('Parsing complex asm file', async () => {
-		// defining URIs for the files used in this test
-		const asmFile = vscode.Uri.file(path.join(__dirname + testFolderLocation + 'test.asm'));
-		const hexFile = vscode.Uri.file(path.join(__dirname + testFolderLocation + 'test_parsed.hex'));
-		const lstFile = vscode.Uri.file(path.join(__dirname + testFolderLocation + 'test_listing.lst'));
-		const parsedFile = vscode.Uri.file(path.join(__dirname + testFolderLocation + 'test.hex'));
-		const createdListingFile = vscode.Uri.file(path.join(__dirname + testFolderLocation + 'test.lst'));
-
-		if(fs.existsSync(parsedFile.fsPath)) {
-			// if an output file is present before we parse one in this test, we remove it
-			fs.unlinkSync(parsedFile.fsPath);
-		}
-		if(fs.existsSync(createdListingFile.fsPath)) {
-			// if an output file is present before we parse one in this test, we remove it
-			fs.unlinkSync(createdListingFile.fsPath);
-		}
-
-		const document = await vscode.workspace.openTextDocument(asmFile);
+		const document = await vscode.workspace.openTextDocument(asmTestFile);
 		const editor = await vscode.window.showTextDocument(document);
 
-		let errorMessage = await vscode.commands.executeCommand('digital-asm.parse-asm', asmFile);
+		let errorMessage = await vscode.commands.executeCommand('digital-asm.parse-asm', asmTestFile);
 
 		assert.ifError(errorMessage);
 
 		// getting the content of the files
-		let parsedContent = fs.readFileSync(parsedFile.fsPath, 'utf-8');
-		const expectedHexContent = fs.readFileSync(hexFile.fsPath, 'utf-8');
-		let listingContent = fs.readFileSync(createdListingFile.fsPath, 'utf-8');
-		const expectedLstContent = fs.readFileSync(lstFile.fsPath, 'utf-8');
+		let parsedContent = standardizeLineEnding(fs.readFileSync(parsedExampleFile.fsPath, 'utf-8'));
+		const expectedHexContent = standardizeLineEnding(fs.readFileSync(hexExampleFile.fsPath, 'utf-8'));
+		let listingContent = standardizeLineEnding(fs.readFileSync(createdExampleListingFile.fsPath, 'utf-8'));
+		const expectedLstContent = standardizeLineEnding(fs.readFileSync(lstExampleFile.fsPath, 'utf-8'));
+		let mappingContent = standardizeLineEnding(fs.readFileSync(createdExampleMappingFile.fsPath, 'utf-8'));
+		const expectedMapContent = standardizeLineEnding(fs.readFileSync(mapExampleFile.fsPath, 'utf-8'));
 
 		assert.deepEqual(parsedContent, expectedHexContent);
 		assert.deepEqual(listingContent, expectedLstContent);
-
-		// cleanup
-		fs.unlinkSync(parsedFile.fsPath); // removes the output file from this test; subsequent tests should start without a parsed .hex file
-		fs.unlinkSync(createdListingFile.fsPath); // removes the output file from this test; subsequent tests should be influenced
+		assert.deepEqual(mappingContent, expectedMapContent);
 	});
 
 });
+
+// replaces all \r\n with \n to end any problems with platform specific line endings when generating the test files
+function standardizeLineEnding(content: string): string {
+	return content.replace(/\r\n/gi, "\n");
+}
