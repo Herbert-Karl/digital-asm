38ecaad39f69b83b0c15ed5e7caebd0fb3fe3c40
Herbert <34774005+Herbert-Karl@users.noreply.github.com>
2021-01-19 22:03:18 +0100

    updated tests for RemoteInterface

--- src/test/suite/remoteInterface.test.ts
+++ src/test/suite/remoteInterface.test.ts
@@ -1,65 +1,63 @@
 import * as assert from 'assert';
 import * as net from 'net';
 import { after, before } from 'mocha';
-import * as path from 'path';
 
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
 import * as vscode from 'vscode';
 import { RemoteInterface } from '../../remoteInterface';
 
-const testFolderLocation = '/../../../src/test/examples/';
-
 suite('RemoteInterface Test Suite', () => {
 	before(() => {
-		vscode.window.showInformationMessage('Start all tests.');
+		vscode.window.showInformationMessage('Start tests for RemoteInterface.');
 	});
 	after(() => {
-		vscode.window.showInformationMessage('All tests done!');
+		vscode.window.showInformationMessage('Finished tests for RemoteInterface!');
 	  });
 
 	test('Sending commands', async () => {
-		// for the test we create our own small tcp listeing server
-		let server = net.createServer();
-		server.listen(27322, "localhost");
-		// we simple answer with the same string for every connection
-		server.on('connection', (socket) => {
-			socket.write("  ok:001f");
+		let simpleTestServer = net.createServer();
+		let hostName = "localhost";
+		let tcpPort = 27322;
+		simpleTestServer.listen(tcpPort, hostName);
+		let staticAnswerString = "  ok:001f";
+		simpleTestServer.on('connection', (socket) => {
+			socket.write(staticAnswerString);
 		});
 
-		let remoteInterface = new RemoteInterface("localhost", 27322);
-		assert.equal(await remoteInterface.step(), 31);
-		assert.equal(await remoteInterface.run(), 31);
+		let remoteInterface = new RemoteInterface(hostName, tcpPort);
+		let expectedAnswer = 31;
+		assert.strictEqual(await remoteInterface.step(), expectedAnswer);
+		assert.strictEqual(await remoteInterface.run(), expectedAnswer);
 
-		// cleanup
-		server.close();
+		simpleTestServer.close();
 	});
 
     test('Sending wrong data', async () => {
-		// for the test we create our own small tcp listeing server
-		let server = net.createServer();
-		server.listen(27323, "localhost");
-		// we simple answer with the same string for every connection
-		server.on('connection', (socket) => {
-			socket.write("this is just wrong");
+		let simpleTestServer = net.createServer();
+		let hostName = "localhost";
+		let tcpPort = 27323;
+		simpleTestServer.listen(tcpPort, hostName);
+		let incorrectSocketMessage = "this is just wrong";
+		simpleTestServer.on('connection', (socket) => {
+			socket.write(incorrectSocketMessage);
 		});
 
-		let remoteInterface = new RemoteInterface("localhost", 27323);
-		let response = await remoteInterface.run().catch((err)=>{ assert.equal(err.message, "Error received from simulator: "+"this is just wrong"); });
+		let remoteInterface = new RemoteInterface(hostName, tcpPort);
+		let response = await remoteInterface.run().catch((err)=>{ assert.strictEqual(err.message, "Error received from simulator: "+incorrectSocketMessage); });
 
 		if(response!==undefined) {
 			assert.fail("the function shouldn't have returned a value");
 		}
 
-		// cleanup
-		server.close();
+		simpleTestServer.close();
 	});
 
 	test('No Server for connection', async () => {
-		const hexFile = path.join(__dirname + testFolderLocation + 'example_parsed.hex');
-
-		let remoteInterface = new RemoteInterface("localhost", 27325);
-		let response = await remoteInterface.run().catch((err)=>{ assert.equal(err.code, "ECONNREFUSED"); });
+		let hostName = "localhost";
+		let tcpPort = 27325;
+		let remoteInterface = new RemoteInterface(hostName, tcpPort);
+		let response = await remoteInterface.run().catch((err)=>{ assert.strictEqual(err.code, "ECONNREFUSED"); });
 
 		if(response!==undefined) {
 			assert.fail("the function shouldn't have returned a value");
